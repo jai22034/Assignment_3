797792987e196d8c427d832e70009876d9433039
jitendra sharma <31501004+jai22034@users.noreply.github.com>
2018-06-07 11:23:01 +0530

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,9 @@
+our_list = []  # create empty list
+
+first_num = int(input('Enter first number: '))
+second_num = int(input('Enter second number: '))
+third_num = int(input('Enter third number: '))
+
+our_list.append(first_num)
+our_list.append(second_num)
+our_list.append(third_num)
